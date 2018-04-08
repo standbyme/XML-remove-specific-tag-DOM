2b66b168d3308591e933e8af7d057bede777de9b
StandbyMe <stand-by-me@mail.com>
2018-04-08 15:09:07 +0800

    recover write

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ soup = BeautifulSoup(open("document.xml"), "xml")
 [s.extract() for s in soup(["mc:AlternateContent","w:drawing"])]
 
 f = open('output.xml', 'w')
-# f.write(soup.prettify())
+f.write(soup.prettify())
 f.close()
