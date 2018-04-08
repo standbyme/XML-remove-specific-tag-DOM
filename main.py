23dfc9cb609d83fafbb599867f27dec62ba6f87a
StandbyMe <stand-by-me@mail.com>
2018-04-08 14:55:27 +0800

    Init

---
+++ main.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(open("document.xml"), "xml")
+[s.extract() for s in soup(["mc:AlternateContent","w:drawing"])]
+
+f = open('output.xml', 'w')
+# f.write(soup.prettify())
+f.close()
